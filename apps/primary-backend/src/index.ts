296f1d8ba7e0349337e07c5ddb3d9f765ad1e7c3
Lalit Kumar Badhotiya <lalitkumar.ac106@gmail.com>
2026-08-22 12:58:53 +0530

    Update index.ts

--- apps/primary-backend/src/index.ts
+++ apps/primary-backend/src/index.ts
@@ -3,6 +3,6 @@ import { cors } from '@elysiajs/cors'
 
 
 app.use(cors({
-    origin: 'http://localhost:3001',
+    origin: 'https://open-router-edv9.onrender.com',
     credentials: true,
 })).listen(3000);
